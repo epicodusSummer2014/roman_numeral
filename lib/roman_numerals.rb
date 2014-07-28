4a33a828a4df0c0aed627139351b43d48f61644f
geh4y8 <guy.halperin88@gmail.com>
2014-07-27 18:10:26 -0700

    change variable names

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,4 +1,5 @@
-def roman_numerals(number)
+def roman_numerals(input)
+	
 	numerals = { 
 		1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 
 		90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 
@@ -7,10 +8,10 @@ def roman_numerals(number)
 
 	result = []
 
- 	numerals.each do |k,v|
- 		if k <= number
- 			result.push(v)
- 			number -= k
+ 	numerals.each do |arabic,roman|
+ 		if arabic <= input
+ 			result.push(roman)
+ 			input -= arabic
  			redo
  		end
  	end
