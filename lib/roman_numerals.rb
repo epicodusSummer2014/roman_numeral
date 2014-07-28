6193d773366135f923ec480a3461bcfeaeb43dca
geh4y8 <guy.halperin88@gmail.com>
2014-07-27 17:45:17 -0700

    pass first two tests

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,20 @@
+def roman_numerals(number)
+	numerals = { 
+		1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 
+		90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 
+		5 => 'V', 4 => 'IV', 1 => 'I'
+	}
+
+  result = []
+
+  numerals.each do |k,v|
+ 	  if k <= number
+ 		  result.push(v)
+ 		  number -= k
+ 		  redo
+ 		end
+  end
+	result.join
+end
+
+p roman_numerals(4)
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('roman_numerals')
 
 describe('roman_numerals') do 
-	it('returns the roman numeral associated with inputted number 1-10') do
-		roman_numerals('3').should(eq('III'))
+	it('returns the roman numeral associated with numbers < 4') do
+		roman_numerals(3).should(eq('III'))
+	end
+	it('returns the roman numerals associated with numbers > 4 < 9') do
+		roman_numerals(4).should(eq('IV'))
 	end
 end
